8b5cf825102bb32255919ddaf2dd6bb7be51b702
s718409 <s718409@MIC-B2002-D28.ky.local>
2020-02-19 15:41:18 +0200

    StaattisiaMetodeja-luokan eka versio

---
+++ Viikko7teht1/src/viikko7teht1/StaattisiaMetodeja.java
@@ -0,0 +1,20 @@
+package viikko7teht1;
+public class StaattisiaMetodeja {
+    
+    public static double ympyranAla(double sade){
+        return sade * sade;
+    }
+
+    public static boolean tarkistaSotu(String sotu){
+        
+        return true;
+    }
+    
+    public static String muunna(String mjono){
+    
+        String jono = mjono;
+        return jono;
+    }
+    
+    
+}
---
+++ Viikko7teht1/src/viikko7teht1/Viikko7teht1.java
@@ -0,0 +1,10 @@
+package viikko7teht1;
+public class Viikko7teht1 {
+    public static void main(String[] args) {
+
+
+        
+
+    }
+    
+}
