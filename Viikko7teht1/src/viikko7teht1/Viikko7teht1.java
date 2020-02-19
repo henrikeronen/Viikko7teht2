65a8d9e4da9f63f63bf0145e1f0196e7daff17cd
Henrik Eronen <eronen.henrik@gmail.com>
2020-02-19 16:19:18 +0200

    System.out.println("Tämä on pääohjelma!");
    /*
    System.out.println("Tämä on pääohjelma!");
    */

--- Viikko7teht1/src/viikko7teht1/Viikko7teht1.java
+++ Viikko7teht1/src/viikko7teht1/Viikko7teht1.java
@@ -3,7 +3,7 @@ public class Viikko7teht1 {
     public static void main(String[] args) {
 
 
-        System.out.println(Math.PI*3.0*3.0);
+        System.out.println("Tämä on pääohjelma!");
     }
     
 }
